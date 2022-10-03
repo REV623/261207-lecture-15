94d25aa676e5305fa40480677c1e386916a660e4
REV623 <kritsadakul@gmail.com>
2022-10-03 15:25:26 +0700

    finished

--- pages/api/todo/[id].js
+++ pages/api/todo/[id].js
@@ -1,7 +1,11 @@
+import checkToken from "../../../backendLibs/checkToken";
 import { readTodolistDB, writeTodolistDB } from "../../../backendLibs/dbLib";
 
 export default function todoIdRoute(req, res) {
   if (req.method === "DELETE") {
+    const username = checkToken(req)
+    if(!username) return res.status(401).json({ok: false, message: "You don't have permission to access this api"})
+
     const todolist = readTodolistDB();
     const id = req.query.id;
 
@@ -10,12 +14,17 @@ export default function todoIdRoute(req, res) {
       return res
         .status(404)
         .json({ ok: false, message: "Todo ID does not exist" });
+        
+    if(todolist[todoIdx].username !== username) return res.status(403).json({ok: false, message: "You don't have permission to access this data"})
 
     todolist.splice(todoIdx, 1);
     writeTodolistDB(todolist);
 
     return res.json({ ok: true, id });
   } else if (req.method === "PUT") {
+    const username = checkToken(req)
+    if(!username) return res.status(401).json({ok: false, message: "You don't have permission to access this api"})
+    
     const todolist = readTodolistDB();
     const id = req.query.id;
 
@@ -28,6 +37,8 @@ export default function todoIdRoute(req, res) {
       return res
         .status(404)
         .json({ ok: false, message: "Todo ID does not exist" });
+
+    if(todolist[todoIdx].username !== username) return res.status(403).json({ok: false, message: "You don't have permission to access this data"})
 
     todolist[todoIdx].completed = req.body.completed;
     writeTodolistDB(todolist);
